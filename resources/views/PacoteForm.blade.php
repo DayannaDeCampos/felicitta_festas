b784ff99c5704c544b7ee2aac42bcee61efdc373
dayanna <dayanna.h@aluno.ifsc.edu.br>
2023-06-23 15:24:35 -0300

    CRUD Pacote

---
+++ resources/views/PacoteForm.blade.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Pacote Form</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+   @php
+        if (!empty($pacote->id)) {
+            $route = route('pacote.update', $pacote->id);
+        } else {
+            $route = route('pacote.store');
+        }
+    @endphp
+        <h1>Formulário de Pacotes</h1>
+
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <form action='{{ $route }}' method="POST" enctype="multipart/form-data">
+            @csrf
+            @if (!empty($pacote->id))
+                @method('PUT')
+            @endif
+
+            <input type="hidden" name="id"
+                value="@if (!empty(old('id'))) {{ old('id') }} @elseif(!empty($pacote->id)) {{ $pacote->id }} @else {{ '' }} @endif" /><br>
+            <div class="col-3">
+                <label class="form-label">Nome</label><br>
+                <input type="text" class="form-control" name="nome"
+                    value="@if (!empty(old('nome'))) {{ old('nome') }} @elseif(!empty($pacote->nome)) {{ $pacote->nome }} @else {{ '' }} @endif" /><br>
+            </div>
+            <div class="col-3">
+                <label class="form-label">Descrição</label><br>
+                <input type="text" class="form-control" name="descricao"
+                    value="@if (!empty(old('descricao'))) {{ old('descricao') }} @elseif(!empty($pacote->descricao)) {{ $pacote->descricao }} @else {{ '' }} @endif" /><br>
+            </div>
+            <div class="col-3">
+                <label class="form-label">Valor</label><br>
+                <input type="text" class="form-control" name="valor"
+                    value="@if (!empty(old('valor'))) {{ old('valor') }} @elseif(!empty($pacote->valor)) {{ $pacote->valor }} @else {{ '' }} @endif" /><br>
+            </div>
+            <div class="col-3">
+                <label class="form-label">Categoria</label><br>
+                <select name="categoriapacote_id" class="form-select">
+                    @foreach ($categoriapacote as $item)
+                        <option value="{{ $item->id }}">{{ $item->nome }}</option>
+                    @endforeach
+                </select>
+            </div>
+            @php
+                $nome_imagem = !empty($pacote->imagem) ? $pacote->imagem : 'sem_imagem.jpg';
+            @endphp
+            <div class="col-6">
+                <br>
+                <img class="img-thumbnail" src="/storage/{{ $nome_imagem }}" width="300px" />
+                <br><br>
+                <input type="file" class="form-control" name="imagem" /><br>
+            </div>
+            <button class="btn btn-dark" type="submit">
+                <i class="fa-solid fa-save"></i> Salvar
+            </button>
+            <a href="{{ route('pacote.index') }}" class="btn btn-dark"><i class="fa-solid fa-arrow-left"></i>
+                Voltar</a>
+        </form>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
